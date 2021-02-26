682385c9b0654dcf5e9e35165ef7ac5783f9605b
vk74 <60939957+fsvk74@users.noreply.github.com>
2021-02-26 13:34:44 +0000

    Create AddonLaunch.sh

---
+++ AddonLaunch.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#REPOSITORIES DISABLED NOW!
+
+clear
+PS3='Please enter your choice: '
+options=("ApkTool (Need Java)" "ApkRepo" "Help" "Exit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "ApkTool (Need Java)")
+         java -jar Addon/apktool.jar
+         echo "Launch this file through terminal manually to use it correctly!"
+	;;
+	"ApkRepo")
+		bash Addon/arepo.sh
+	;;
+	"Help")
+		bash Addon/hman.sh
+	;;
+	"Exit")
+            break
+        ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
